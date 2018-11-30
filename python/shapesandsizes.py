d4f7c455febcb4641f28ff65c838cf0217b4c79c
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2018-11-29 21:26:11 -0500

    shapefiles

--- python/shapesandsizes.py
+++ python/shapesandsizes.py
@@ -1,16 +1,15 @@
 import shapefile
 import numpy as np
-import matplotlib as plt
+from matplotlib import pyplot as plt
 sf = shapefile.Reader('/Users/francopettigrosso/ws/redisticting_game/python/shapefiles/tr42_d00')
 #first feature of the shapefile
 print(f'number of shapes imported:{len(sf.shapes())}')
 
-shape_ex = sf.shape(5)
-
-x_lon = np.zeros((len(shape_ex.points),1))
-y_lat = np.zeros((len(shape_ex.points),1))
-for ip in range(len(shape_ex.points)):
-    x_lon[ip] = shape_ex.points[ip][0]
-    y_lat[ip] = shape_ex.points[ip][1]
-
-plt.pyplot.plot(x_lon,y_lat)
+for shape_ex in sf.shapes():
+    x_lon = np.zeros((len(shape_ex.points),1))
+    y_lat = np.zeros((len(shape_ex.points),1))
+    for ip in range(len(shape_ex.points)):
+        x_lon[ip] = shape_ex.points[ip][0]
+        y_lat[ip] = shape_ex.points[ip][1]
+    plt.plot(x_lon,y_lat)
+print('done')

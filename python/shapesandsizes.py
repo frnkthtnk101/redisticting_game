c7d360687c8b5d501552fea0384c3e6cceaea336
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2018-12-01 12:37:46 -0500

    have workign prototype. Waiting for csv.

--- python/shapesandsizes.py
+++ python/shapesandsizes.py
@@ -1,57 +1,89 @@
 #https://chrishavlin.com/2016/11/16/shapefiles-tutorial/
 #https://gis.stackexchange.com/questions/131716/plot-shapefile-with-matplotlib
 #https://stackoverflow.com/questions/30447790/displaying-a-shapefile/48056459#48056459
+#https://chrishavlin.com/2016/12/01/shapely-polygons-coloring/
 import shapefile
 import numpy as np
+import csv
 from matplotlib import pyplot as plt
 from descartes import PolygonPatch
+from shapely.geometry import Polygon
 sf = shapefile.Reader('/Users/francopettigrosso/ws/redisticting_game/python/shapefiles/tr42_d00')
 #first feature of the shapefile
+tract_info = []
+with open('/Users/francopettigrosso/ws/redisticting_game/python/districts.csv','r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        tract_info.append(row)
+
+colors = [
+    '#02ff85',
+    '#ff027c',
+    '#ff8502',
+    '#027cff',
+    '#0004ff',
+    '#fffb00',
+    '#04ff00',
+    '#fb00ff',
+    '#a5a7ff',
+    '#fffea5',
+    '#a7ffa5',
+    '#fea5ff',
+    '#ffacda',
+    '#acffd3',
+    '#acdaff',
+    '#ffd3ac',
+    '#ff01fb',
+    '#ff0901',
+    '#01fbff',
+    '#01ff09'
+
+]
+
 print(f'number of shapes imported:{len(sf.shapes())}')
+length = len(sf.fields)
+for i in range(length):
+    print(i,sf.fields[i][0])
 
 
 plt.figure()
-t =plt.gca()
-i =1
-for shape in list(sf.iterShapes()):
-    npoints = len(shape.points)
+ax = plt.axes()
+ax.set_aspect('equal')
+
+    
+i = 0
+for shaperec in list(sf.iterShapeRecords()):
+    shape = shaperec.shape
+    rec = shaperec.record
+    print(f"state: {rec[4]} county: {rec[5]} tract: {rec[6]} Name: {rec[7]}")
     nparts = len(shape.parts)
-    poly = shape.__geo_interface__
-    if i % 6 == 0:
-        color = '#663300'
-    elif i %5 == 0:
-        color = '#663300'
-    elif i %4 == 0:
-        color = '#663300'
-    elif i %3 == 0:
-        color = '#cc0066'
-    elif i %2 == 0:
-        color = '#cc0066'
-    else:
-        color = '#cc0066'
-    i+=1
-    t.add_patch(PolygonPatch(poly, facecolor=[0,0,0.5],edgecolor=[0,0,0], alpha=0.7, zorder=2))
-    t.axis('equal')
     if nparts == 1:
-        x_lon = np.zeros((len(shape.points),1))
-        y_lat = np.zeros((len(shape.points),1))
-        for ip in range(len(shape.points)):
-            x_lon[ip] = shape.points[ip][0]
-            y_lat[ip] = shape.points[ip][1]
-        plt.plot(x_lon,y_lat)
+        polygon = Polygon(shape.points)
+        patch = PolygonPatch(polygon,facecolor=colors[i],alpha=1,zorder=1)
+        patch.set_linewidth(None)
+        #patch.set_edgecolor('none')
+        patch.set_edgecolor(colors[i])
+        ax.add_patch(patch)
     else:
         for ip in range(nparts):
             i0=shape.parts[ip]
-            if ip < nparts-1:
+            if ip < nparts - 1:
                 i1 = shape.parts[ip+1]-1
             else:
-                i1 = npoints
-            seg = shape.points[i0:i1+1]
-            x_lon = np.zeros((len(seg),1))
-            y_lat = np.zeros((len(seg),1))
-            for ip in range(len(seg)):
-                x_lon[ip] = seg[ip][0]
-                y_lat[ip] = seg[ip][1]
+                i1 = len(shape.points)
+            polygon = Polygon(shape.points[i0:i1+1])
+            patch = PolygonPatch(polygon,facecolor=colors[i],alpha = 1 , zorder = 1)
+            patch.set_linewidth(None)
+            #patch.set_edgecolor('none')
+            patch.set_edgecolor(colors[i])
+            ax.add_patch(patch)
+    if (i < 18):
+        i+= 1
+    else:
+        i=0
 
-        plt.plot(x_lon,y_lat)
+
+
+plt.xlim(-81,-74.5)
+plt.ylim(39.7,42.5)
 plt.show()
